68a7268622d6e24fbbf6664f03a88e73c8deb4d7
monticelli.ricardo <monticelli19@gmail.com>
2024-06-27 12:59:39 +0200

    Address GraphQL requests (@QueryMapping).

--- src/main/java/lu/uni/binfocep/javaee/exercise3/QueryResolver.java
+++ src/main/java/lu/uni/binfocep/javaee/exercise3/QueryResolver.java
@@ -1,28 +1,46 @@
 package lu.uni.binfocep.javaee.exercise3;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
 
+@Controller
 @Component
 public class QueryResolver implements GraphQLQueryResolver {
 
+    // Properties
     private final Covid19DataService covid19DataService;
 
+    // Constructor
     @Autowired
     public QueryResolver(Covid19DataService covid19DataService) {
         this.covid19DataService = covid19DataService;
     }
 
-    public List<Covid19Data> getCovid19DataByCountry(List<String> countries, List<String> selectedFields) throws IOException {
-        return covid19DataService.getCovid19DataByCountry(countries, selectedFields);
+
+    // For GraphiQL interface
+
+    // Method to return COVID19 data based on one or several country name(s) and selected subfields
+    @QueryMapping
+    public List<Map<String, Object>> getCovid19DataByCountry(@Argument List<String> countries) {
+        List<Map<String, Object>> allData = covid19DataService.fetchAllData();
+        return allData.stream()
+                .filter(data -> countries.contains(data.get("country")))
+                .collect(Collectors.toList());
     }
 
-    public List<String> getAvailableCountries() throws IOException  {
-        return covid19DataService.getAvailableCountries();
+    // Method to return COVID19 based on selected subfields
+    @QueryMapping
+    public List<Map<String, Object>> getCovid19Data() {
+        return covid19DataService.fetchAllData();
     }
+    
 }
